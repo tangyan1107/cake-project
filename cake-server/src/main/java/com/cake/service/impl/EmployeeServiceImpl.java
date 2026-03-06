c3eb8e66e21b95ea918d6f19cbbf73db6fdc955f
tangyan1107 <3135416656@qq.com>
2026-03-06 16:49:09 +0800

    编辑员工业务代码开发

--- cake-server/src/main/java/com/cake/service/impl/EmployeeServiceImpl.java
+++ cake-server/src/main/java/com/cake/service/impl/EmployeeServiceImpl.java
@@ -136,4 +136,29 @@ public class EmployeeServiceImpl implements EmployeeService {
         employeeMapper.update(employee);
     }
 
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Override
+    public Employee getById(Long id) {
+        Employee employee = employeeMapper.getById(id);
+        employee.setPassword("****");
+        return employee;
+    }
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    @Override
+    public void edit(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+        BeanUtils.copyProperties(employeeDTO,employee);//对对象属性拷贝
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employee.setUpdateTime(LocalDateTime.now());
+        employeeMapper.update(employee);
+    }
+
 }
