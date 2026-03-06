4c178e3892d0239ebefd20ffd595e2c6713ab440
tangyan1107 <3135416656@qq.com>
2026-03-06 14:51:13 +0800

    启用,禁用员工部分基本完成

--- cake-server/src/main/java/com/cake/service/impl/EmployeeServiceImpl.java
+++ cake-server/src/main/java/com/cake/service/impl/EmployeeServiceImpl.java
@@ -113,4 +113,27 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(page.getTotal(),records);
     }
 
+    /**
+     * 启用,禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+     //update employee set status = ? where id = ?
+        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        /*
+        * Employee employee = Employee.builder()
+        *           .status(status)
+        *           .id(id)
+        *           .build();
+        * */
+
+        employeeMapper.update(employee);
+    }
+
 }
